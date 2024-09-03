ec016671da8163cdc44cdd527e00d4ffb31c05c5
Kiril Baskakov <kirillbaskakov15@gmail.com>
2024-09-03 22:07:42 +0300

    add: observer

--- src/components/TweetsList/TweetsList.tsx
+++ src/components/TweetsList/TweetsList.tsx
@@ -2,7 +2,7 @@ import { getTweets } from '@/api/tweets';
 import Tweet from '@/components/Tweet/Tweet';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import { TweetType } from '@/types/TweetType';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const TweetsList = ({
   onlyUserTweets = false
@@ -11,11 +11,32 @@ const TweetsList = ({
 }) => {
   const currentUser = useCurrentUser();
   const [tweets, setTweets] = useState<Array<TweetType>>([]);
+  const ref = useRef(null);
 
   const getData = async () => {
     const tweets = await getTweets(currentUser!.id, onlyUserTweets);
     setTweets(tweets);
   };
+
+  const onIntersect = () => {
+    console.log(1);
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(onIntersect, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.7
+    });
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref]);
 
   useEffect(() => {
     if (currentUser) getData();
@@ -23,8 +44,10 @@ const TweetsList = ({
 
   return (
     <div className="flex flex-col gap-4 mt-4">
-      {tweets.map(tweet => (
-        <Tweet key={tweet.id} {...tweet} />
+      {tweets.map((tweet, index) => (
+        <div ref={index + 5 == tweets.length ? ref : null}>
+          <Tweet key={tweet.id} {...tweet} />
+        </div>
       ))}
     </div>
   );
